1a0c40a19602106efd7059cebfa6f0ed9e9d9b19
rhellhbn <164168446+rhellhbn@users.noreply.github.com>
2026-03-02 17:48:14 +0700

    Rahel

--- src/main/kotlin/services/ProfileService.kt
+++ src/main/kotlin/services/ProfileService.kt
@@ -13,8 +13,8 @@ class ProfileService {
             "success",
             "Berhasil mengambil profile pengembang",
             mapOf(
-                Pair("username", "abdullah.ubaid"),
-                Pair("nama", "Abdullah Ubaid"),
+                Pair("username", "ifs23049"),
+                Pair("nama", "Rahel Debora Hasibuan"),
                 Pair("tentang", "Saya adalah seorang developer yang tertarik pada mobile development, backend API, dan berbagai teknologi pengembangan aplikasi. Senang belajar hal baru dan membangun aplikasi yang berguna."),
             )
         )
